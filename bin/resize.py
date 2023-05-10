11a5e27251190789784a337e062d524b00ee16e0
glyn-stevens <glynst@gmail.com>
2023-05-10 16:52:54 +0100

    Add script to crop and resize images and generate cropped and resized images

---
+++ bin/resize.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from PIL import Image
+from pathlib import Path
+
+root_path = Path.cwd()
+dir = root_path / "data" / "original"
+images = Path(dir).glob('*.jpg')
+out_dir = root_path / "data" / "resized"
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+
+for path in Path(dir).iterdir():
+    if path.is_file() and path.suffix == '.jpg':
+        im = Image.open(path)
+        im_cropped = crop_max_square(im)
+        f_name = path.name
+        imResize = im_cropped.resize((200,200), Image.ANTIALIAS)
+        imResize.save(out_dir/(f_name + '_resized.jpg'), 'JPEG', quality=90)
+
+
+    
