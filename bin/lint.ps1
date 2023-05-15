d80cf60870a18ca8f9966119e2498a15a0dc9ee8
glyn-stevens <glynst@gmail.com>
2023-05-15 14:43:10 +0100

    Add pytest - make linting run on tests folder too

--- bin/lint.ps1
+++ bin/lint.ps1
@@ -1,11 +1,14 @@
 "`nisort:"
-isort src
-
-"`nflake8:"
-flake8 src --max-line-length=99
+isort src tests
 
 "`nblack:"
-black -t py310 -l 79 src
+black -t py310 -l 79 src tests
 
-"`nmypy:"
+"`nflake8:"
+flake8 src tests --max-line-length=99
+
+"`nmypy src:"
 mypy src --ignore-missing-imports --disallow-untyped-defs
+
+"`nmypy tests:"
+mypy tests --ignore-missing-imports
