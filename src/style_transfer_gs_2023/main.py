1760821e4cf4719c9594f83373075d5b28aef41b
glyn-stevens <glynst@gmail.com>
2023-05-15 12:07:43 +0100

    Create main file which loads and displays an image

---
+++ src/style_transfer_gs_2023/main.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from style_transfer_gs_2023 import ROOT_PATH
+
+
+def style_transfer() -> None:
+    resized_imgs_path = ROOT_PATH / "data" / "resized"
+    content_image = Image.open(resized_imgs_path / "content_resized.jpg")
+    style_image = Image.open(resized_imgs_path / "portrait_arden_resized.jpg")
+    content_image.show()
+
+if __name__ == "__main__":
+    style_transfer()
+
